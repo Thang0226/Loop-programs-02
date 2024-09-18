71cde0e4d3a73df5831b48d4e0e51219d6ebcb25
Thang0226 <thang.nd0226@gmail.com>
2024-09-18 16:09:34 +0700

    update for() loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,7 @@
 let times = +prompt("Nhập vào số số fibonacci muốn in ra:");
 let loop_times = Math.floor(times / 2);
 
-let num1 = 1;
-let num2 = 1;
-
-for (let i = 0; i < loop_times; i++) {
+for (let i = 0, num1 = 1, num2 = 1; i < loop_times; i++) {
   document.write(num1 + "<br>");
   document.write(num2 + "<br>");
   num1 = num1 + num2;
